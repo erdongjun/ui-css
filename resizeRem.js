7a3483cd09c320d590a90b3549c8e1629444a091
Davau <Davau@users.noreply.github.com>
2016-06-29 17:48:41 +0800

    Update resizeRem.js

--- resizeRem.js
+++ resizeRem.js
@@ -1,6 +1,6 @@
 
 //rem单位配合js 自适应各分辨率手机
-// 注意 必须在head 中加载此文件  若在body后加载 刷新页面则会出现元素单位值改变的现象
+// 注意 必须在head 中加载此文件  若在body后加载 刷新页面则会出现元素单位值改变后闪屏的现象.
 
 function infinite() {
     var htmlWidth = (document.body && document.body.clientWidth || document.getElementsByTagName("html")[0].offsetWidth)
